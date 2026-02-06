94824087820e4862cd81c39110dbf77446c02ee7
Jay Herron <NeedleInAJayStack@protonmail.com>
2026-02-06 00:39:57 -0700

    refactor: removes unnecessary initializer

--- Sources/GraphQLVapor/GraphQLHandler.swift
+++ Sources/GraphQLVapor/GraphQLHandler.swift
@@ -9,16 +9,4 @@ struct GraphQLHandler<
     let rootValue: any Sendable
     let config: GraphQLConfig<WebSocketInit>
     let computeContext: @Sendable (Request) async throws -> Context
-
-    init(
-        schema: GraphQLSchema,
-        rootValue: any Sendable,
-        config: GraphQLConfig<WebSocketInit>,
-        computeContext: @Sendable @escaping (Request) async throws -> Context
-    ) {
-        self.schema = schema
-        self.rootValue = rootValue
-        self.config = config
-        self.computeContext = computeContext
-    }
 }
